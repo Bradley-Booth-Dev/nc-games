233f20c6473b3172a17890fb96c6bde01b2daaf3
Brad <fullenglish3@hotmail.co.uk>
2023-04-20 12:17:23 +0100

    ammendments added

--- src/components/Single-review.jsx
+++ src/components/Single-review.jsx
@@ -37,26 +37,30 @@ export default function SingleReview() {
       </section>
 
       <section className="singleReviewComments">
-        <ol>
-          Comments:
-          {comments.map((comment) => {
-            const createdAtDate = new Date(comment.created_at);
-            const createdAtString = createdAtDate.toLocaleDateString();
-            const createdAtTimeString = createdAtDate.toLocaleTimeString();
-            const comment_id = comment.comment_id;
+        {comments.length === 0 ? (
+          <p>No comments</p>
+        ) : (
+          <ol>
+            Comments:
+            {comments.map((comment) => {
+              const createdAtDate = new Date(comment.created_at);
+              const createdAtString = createdAtDate.toLocaleDateString();
+              const createdAtTimeString = createdAtDate.toLocaleTimeString();
+              const comment_id = comment.comment_id;
 
-            return (
-              <li className="singleComment" key={comment_id}>
-                <h4>Username:{comment.author}</h4>
-                <h5>{comment.body}</h5>
-                <h6>Votes: {comment.votes}</h6>
-                <h6>
-                  Posted at: {createdAtString} at {createdAtTimeString}
-                </h6>
-              </li>
-            );
-          })}
-        </ol>
+              return (
+                <li className="singleComment" key={comment_id}>
+                  <h4>Username:{comment.author}</h4>
+                  <h5>{comment.body}</h5>
+                  <h6>Votes: {comment.votes}</h6>
+                  <h6>
+                    Posted at: {createdAtString} at {createdAtTimeString}
+                  </h6>
+                </li>
+              );
+            })}
+          </ol>
+        )}
       </section>
     </div>
   );
